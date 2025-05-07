cefc8a3b5d1bd73aa981451759524aad3ae44743
eliasschae <elias-schaefer1@web.de>
2025-05-07 10:21:31 -0300

    Deine Commit-Nachricht hier

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -14,6 +15,26 @@ import { TranslationService } from '../services/translation.service';
 export class ContactComponent {
   public text: { [key: string]: string } = {};
   
+  reloadPage() {
+    const privacySection = this.elRef.nativeElement.querySelector('#privacy-policy');
+  
+    if (privacySection) {
+      privacySection.classList.add('hidden');
+    }
+  
+    setTimeout(() => {
+      window.location.reload();
+    }, 300);
+  }
+  
+  openPrivacyPolicy(event: Event) {
+    event.preventDefault();
+    const section = this.elRef.nativeElement.querySelector('#privacy-policy');
+    if (section) {
+      section.classList.add('show');
+    }
+  }  
+
   constructor(private translationService: TranslationService, private elRef: ElementRef) {
       this.translationService.text$.subscribe((text) => {
         this.text = text;
